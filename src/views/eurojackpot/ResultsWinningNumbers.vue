46d5e342ac3ab6f24a1abf053b28613c2380f2b8
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-01 18:16:02 -0300

    Basic markup for results-winning-numbers

--- src/views/eurojackpot/ResultsWinningNumbers.vue
+++ src/views/eurojackpot/ResultsWinningNumbers.vue
@@ -1,13 +1,124 @@
 <template>
   <div>
+    <!-- <b-row class="mt-5 mb-3 mt-sm-5 mt-md-3 subMenu">
+      <b-col sm="12" md="4" lg="4">
+        <b-button block>EuroJackpot</b-button>
+      </b-col>
+      <b-col sm="12" md="4" lg="4">
+        <b-button block>EuroJackpot Results & Prize Breakdown</b-button>
+      </b-col>
+      <b-col sm="12" md="4" lg="4">
+        <b-button block>EuroJackpot Help & FAQ</b-button>
+      </b-col>
+    </b-row> -->
+
     <h1>EUROJACKPOT RESULTS & WINNING NUMBERS</h1>
-    <!-- submenu -->
-    <!-- page title -->
     <!-- inputs date -->
     <!-- result date -->
     <!-- numbers -->
-    <!-- table -->
-    <!-- side cards -->
+
+    <b-row>
+      <b-col sm="12" md="7" lg="8">
+        <b-table-simple responsive>
+          <b-thead>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Header</b-th>
+              <b-th>Heading 1</b-th>
+              <b-th>Heading 2</b-th>
+              <b-th>Heading 3</b-th>
+              <b-th>Heading 4</b-th>
+            </b-tr>
+          </b-thead>
+          <b-tbody>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+            <b-tr>
+              <b-th sticky-column>Sticky Column Row Header</b-th>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+              <b-td>Cell</b-td>
+            </b-tr>
+          </b-tbody>
+        </b-table-simple>
+      </b-col>
+      <b-col m="12" md="5" lg="4">
+        <b-card>
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
