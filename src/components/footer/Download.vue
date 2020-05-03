b8f795bc4c305e54d4b8a9ba9add4483810cd759
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-03 20:32:56 -0300

    Footer finished

--- src/components/footer/Download.vue
+++ src/components/footer/Download.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>Footer downloads</div>
+  <b-container>
+    <b-row class="social">
+      <b-col>
+        <a
+          href="http://app.adjust.com/2abihdx"
+          title="Free on iOS"
+          target="_blank"
+          class="l-link-basic"
+        >
+          <img
+            width="71"
+            height="24"
+            class="footer-img lazyloaded"
+            src="@/assets/images/webapp_apps_btn_ios.png"
+        /></a>
+
+        <a
+          href="http://app.adjust.com/nv5ih75"
+          title="Free on Android"
+          target="_blank"
+          class="l-link-basic"
+        >
+          <img
+            width="63"
+            height="24"
+            class="footer-img lazyloaded"
+            src="@/assets/images/webapp_apps_btn_ios.png"
+        /></a>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.social {
+  text-align: center;
+  margin: 72px 0;
+
+  a {
+    margin: 0 10px;
+  }
+}
+</style>
--- src/components/footer/Signs.vue
+++ src/components/footer/Signs.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>Footer signs</div>
+  <b-container>
+    <p>
+      EU Lotto Limited, of Suite A, Ocean Village Promenade, Ocean Village,
+      Gibraltar GX11 1AA, is licensed by the Government of Gibraltar and
+      regulated by the Gibraltar Gambling Commissioner under the Gibraltar
+      Gambling Act 2005 (Licences:
+      <a href="https://www.gibraltar.gov.gi/new/remote-gambling#ancla2"
+        >RGL 085 & 066</a
+      >), and is licensed and regulated by the Gambling Commission of Great
+      Britain for customers in Great Britain (Licence:
+      <a
+        href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/38991"
+        >000-038991-R-319408-005</a
+      >). EU Lotto Limited has a Remote Bookmaker’s Licence issued by the Irish
+      National Excise Licence Office (Reference Number 1011284) for lotto
+      betting activities in the Republic of Ireland.
+    </p>
+
+    <p>
+      This website is operated by Lottoland. Lottoland passes on Player’s Bets
+      to EU Lotto, which is acting as a bookmaker. When placing Bets, Lottoland
+      acts in the name and on behalf of the Player.
+    </p>
+
+    <p>
+      Persons under 18 years of age are not permitted to gamble.
+    </p>
+
+    <p>
+      This is a real money gambling app/site. Please gamble safely and only bet
+      what you can afford. For gambling addiction help and support, please
+      contact Gamble Aware at 0808 8020 133 or visit
+      <a href="https://www.begambleaware.org/"
+        >https://www.begambleaware.org/.</a
+      >
+    </p>
+
+    <p>
+      EuroJackpot - check the official results and winning numbers
+    </p>
+  </b-container>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+p {
+  margin-bottom: 36px;
+  font-size: 12px;
+  color: #012406;
+  font-weight: 200;
+  text-align: center;
+
+  a {
+    font-weight: 700;
+  }
+}
+</style>
